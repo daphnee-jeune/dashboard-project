07b8b7b3321cc0baf0faff2e46d0a7d1d48a955a
Daphnee Jeune <djeune@newrelic.com>
2023-06-01 23:41:13 -0500

    add navbar functionality

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,25 +1,27 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from "react";
 
-const StateContext = createContext()
+const StateContext = createContext();
 
 const initialState = {
   chat: false,
   cart: false,
   userProfile: false,
-  notification: false
-}
+  notification: false,
+};
 
 export const ContextProvider = ({ children }) => {
-  const [activeMenu, setActiveMenu] = useState(true)
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
 
-
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true });
+  };
   return (
     <StateContext.Provider
-      value={{ activeMenu, setActiveMenu }}
-    >
+      value={{ activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick }}>
       {children}
     </StateContext.Provider>
-  )
-}
+  );
+};
 
-export const useStateContext = () => useContext(StateContext)
+export const useStateContext = () => useContext(StateContext);
